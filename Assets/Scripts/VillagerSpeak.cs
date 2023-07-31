a705ba7447a8abdb03e49485d8563bdb90eb5b9e
laurenc06 <139818583+laurenc06@users.noreply.github.com>
2023-07-31 11:42:31 -0700

    ppl added wowowowowowowo

---
+++ Assets/Scripts/VillagerSpeak.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerSpeak : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.name == "Billy") {
+            Debug.Log("Only one of us will be telling the truth.\nGiana was heading to the berries carrying a fruit basket.");
+        }
+        if (other.name == "Lisa") {
+            Debug.Log("Giana hates the heat.\nShe would never go to the desert.");
+        }
+        if (other.name == "Leo") {
+            Debug.Log("Only one of us are lying.\nGiana was wearing a snow jacket before she left.");
+        }
+        if (other.name == "Greg") {
+            Debug.Log("Billy is telling the truth, and Giana was wearing a tanktop and shorts.");
+        }
+        if (other.name == "Faith") {
+            Debug.Log("Lisa is telling the truth.\nI heard Giana has a vacation home on the other side of Fairy Forest.");
+        }
+
+    }
+}
