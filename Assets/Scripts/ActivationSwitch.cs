bd89abc44bc06c8a29b87a469dac5834548230db
Avani Agrawal <avaniagrawal00@gmail.com>
2023-08-01 11:51:08 -0700

    Update ActivationSwitch.cs

--- Assets/Scripts/ActivationSwitch.cs
+++ Assets/Scripts/ActivationSwitch.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ActivationSwitch : MonoBehaviour
 {
+    public bool active;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        active = false;
     }
 
     // Update is called once per frame
