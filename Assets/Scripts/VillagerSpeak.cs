36fb766cc41e63f00ef17e62e0dc643b3cea0c8f
Avani Agrawal <avaniagrawal00@gmail.com>
2023-08-01 10:12:27 -0700

    Villager Speak GUI

--- Assets/Scripts/VillagerSpeak.cs
+++ Assets/Scripts/VillagerSpeak.cs
@@ -1,33 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class VillagerSpeak : MonoBehaviour
 {
+    string infoText;
+    TMP_Text info;
+    bool billy;
+    bool lisa;
+    bool leo;
+    bool greg;
+    bool faith;
+    
     // Start is called before the first frame update
     void Start()
     {
+        infoText = "";
+        info = GameObject.Find("Information").GetComponent<TMP_Text>();
+        billy = true;
+        lisa = true;
+        leo = true;
+        greg = true;
+        faith = true;
     }
     // Update is called once per frame
     void Update()
-    {   
+    {
+        
     }
 
     void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Billy")) {
-            Debug.Log("Billy: Only one of us will be telling the truth. Giana was heading to the berries carrying a fruit basket.");
+        if (billy && other.CompareTag("Billy")) {
+            infoText += "\n\nBilly: Only one of us will be telling the truth. Giana was heading to the berries carrying a fruit basket";
+            billy = false;
         }
-        if (other.CompareTag("Lisa")) {
-            Debug.Log("Lisa: Giana hates the heat. She would never go to the desert.");
+        if (lisa && other.CompareTag("Lisa")) {
+            infoText += "\n\nLisa: Giana hates the heat. She would never go to the desert.";
+            lisa = false;
         }
-        if (other.CompareTag("Leo")) {
-            Debug.Log("Leo: Only one of us are lying. Giana was wearing a snow jacket before she left.");
+        if (leo && other.CompareTag("Leo")) {
+            infoText += "\n\nLeo: Only one of us are lying. Giana was wearing a snow jacket before she left.";
+            leo = false;
         }
-        if (other.CompareTag("Greg")) {
-            Debug.Log("Greg: Billy always tells the truth, and Giana was wearing a tanktop and shorts.");
+        if (greg && other.CompareTag("Greg")) {
+            infoText += "\n\nGreg: Billy always tells the truth, and Giana was wearing a tanktop and shorts.";
+            greg = false;
         }
-        if (other.CompareTag("Faith")) {
-            Debug.Log("Faith: Lisa never lies. I heard Giana has a vacation home on the other side of Fairy Forest.");
+        if (faith && other.CompareTag("Faith")) {
+            infoText += "\n\nFaith: Lisa never lies. I heard Giana has a vacation home on the other side of Fairy Forest.";
+            faith = false;
         }
+        info.text = infoText;
     }
 }
